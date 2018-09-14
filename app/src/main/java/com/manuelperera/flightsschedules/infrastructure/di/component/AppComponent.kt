69d474b86cf77a30b58ef036538f2e3442b88683
Manuel Perera <manuel.perera@innocv.com>
2018-09-14 14:14:30 +0200

    Remove unused Module and add provides for Presenter in Android Tests

--- app/src/main/java/com/manuelperera/flightsschedules/infrastructure/di/component/AppComponent.kt
+++ app/src/main/java/com/manuelperera/flightsschedules/infrastructure/di/component/AppComponent.kt
@@ -2,18 +2,32 @@ package com.manuelperera.flightsschedules.infrastructure.di.component
 
 import com.manuelperera.flightsschedules.domain.repository.LoginRepository
 import com.manuelperera.flightsschedules.infrastructure.FlightsApp
-import com.manuelperera.flightsschedules.infrastructure.di.module.AppModule
+import com.manuelperera.flightsschedules.infrastructure.di.module.ActivityModule
+import com.manuelperera.flightsschedules.infrastructure.di.module.RepositoryModule
+import com.manuelperera.flightsschedules.infrastructure.di.module.ServiceModule
+import com.manuelperera.flightsschedules.presentation.schedulelist.ScheduleListPresenter
+import com.manuelperera.flightsschedules.presentation.selectairport.AirportListPresenter
 import com.manuelperera.flightsschedules.presentation.splash.SplashPresenter
 import dagger.Component
 import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class])
+@Component(modules = [
+    AndroidSupportInjectionModule::class,
+    ActivityModule::class,
+    RepositoryModule::class,
+    ServiceModule::class
+])
 interface AppComponent : AndroidInjector<FlightsApp> {
 
     fun provideLoginRepository(): LoginRepository
 
     fun provideSplashPresenter(): SplashPresenter
+
+    fun provideSchedulesListPresenter(): ScheduleListPresenter
+
+    fun provideAirportsListPresenter(): AirportListPresenter
 
 }
